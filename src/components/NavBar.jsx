0266ac4554bead30b0b1d7a48414597a698f0dee
Manish0258 <manishgupta56777@gmail.com>
2025-08-31 19:16:15 +0530

    improve navbar name

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -24,7 +24,7 @@ const NavBar=() =>{
 
     return  <div className="navbar bg-base-100 shadow-sm">
   <div className="flex-1">
-    <Link to="/" className="btn btn-ghost text-xl">🧑‍💻DevTinder</Link>
+    <Link to="/" className="btn btn-ghost text-xl">🧑‍💻DevMeet</Link>
   </div>
   
   <div className="flex gap-2">
